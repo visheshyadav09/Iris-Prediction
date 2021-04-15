9d9ef07a700a7b268b29f47df7b5fc9625ec902e
visheshyadav09 <61138952+visheshyadav09@users.noreply.github.com>
2021-04-15 11:10:52 +0700

    init

---
+++ predict/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class PredResults(models.Model):
+
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField()
+    classification = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.classification
